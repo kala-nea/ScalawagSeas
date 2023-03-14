cfbff2220dd4958a217d0ba283f285726f69a9a7
kala-nea <116219429+kala-nea@users.noreply.github.com>
2023-03-14 15:46:17 -0700

    a few more fixes

--- JS/LocalSetup.js
+++ JS/LocalSetup.js
@@ -1,16 +1,15 @@
 let PlayerCountIn = document.getElementById("PlayerCount");
 let BoatCountIn = document.getElementById("BoardCount");
 
+if (window.localStorage.getItem('PlayerCount') == null||window.localStorage.getItem('PlayerCount') == "") {
+    window.localStorage.setItem('PlayerCount', 2);
+}
+if (window.localStorage.getItem('BoardCount') == null||window.localStorage.getItem('BoardCount') == "") {
+    window.localStorage.setItem('BoardCount', 1);
+}
 
 PlayerCountIn.value=window.localStorage.getItem('PlayerCount');
 BoatCountIn.value=window.localStorage.getItem('BoatCount');
-
-if (PlayerCountIn.value == 0) {
-    PlayerCountIn.value = 2;
-}
-if (BoatCountIn.value == 0) {
-    BoatCountIn.value = 1;
-}
 
 PlayerCountIn.addEventListener("change",setPlayers);
 BoatCountIn.addEventListener("change",setBoats);
@@ -22,19 +21,19 @@ function setBoats(){
     window.localStorage.setItem('BoatCount',BoatCountIn.value);
 }
 
+
 let BoardXIn = document.getElementById("BoardX");
 let BoardYIn = document.getElementById("BoardY");
 
+if (window.localStorage.getItem('BoardX') == null||window.localStorage.getItem('BoardX') == "") {
+    window.localStorage.setItem('BoardX', 34);
+}
+if (window.localStorage.getItem('BoardY') == null||window.localStorage.getItem('BoardY') == "") {
+    window.localStorage.setItem('BoardY', 16);
+}
 
 BoardXIn.value=window.localStorage.getItem('BoardX');
 BoardYIn.value=window.localStorage.getItem('BoardY');
-
-if (BoardXIn.value == 0) {
-    BoardXIn.value = 34;
-}
-if (BoardYIn.value == 0) {
-    BoardYIn.value = 16;
-}
 
 BoardXIn.addEventListener("change",setX);
 BoardYIn.addEventListener("change",setY);
@@ -46,19 +45,19 @@ function setY(){
     window.localStorage.setItem('BoardY',BoardYIn.value);
 }
 
+
 let IslandIn = document.getElementById("IslandCount");
 let RocksIn = document.getElementById("RockCount");
 
+if (window.localStorage.getItem('IslandCount') == ""||window.localStorage.getItem('IslandCount') == null) {
+    window.localStorage.setItem('IslandCount', 8);
+}
+if (window.localStorage.getItem('RockCount') == ""||window.localStorage.getItem('RockCount') == null) {
+    window.localStorage.setItem('RockCount', 16);
+}
 
 IslandIn.value=window.localStorage.getItem('IslandCount');
 RocksIn.value=window.localStorage.getItem('RockCount');
-
-if (IslandIn.value == 0) {
-    IslandIn.value = 2;
-}
-if (RocksIn.value == 0) {
-    RocksIn.value = 1;
-}
 
 IslandIn.addEventListener("change",setIs);
 RocksIn.addEventListener("change",setRk);
