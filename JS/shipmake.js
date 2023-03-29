3018e2926d9eb0f02dc5b71792bb89e4618a2153
kala-nea <116219429+kala-nea@users.noreply.github.com>
2023-03-29 15:50:49 -0700

    base stats dont show

--- JS/shipmake.js
+++ JS/shipmake.js
@@ -37,8 +37,8 @@ for (ton in statsByTon) {
     tonnageTable.appendChild(newRow);
 }
 
+
 // get all the elements that will need to be edited
-let shipName = document.getElementById("shipName");
 let wcDisplay = document.getElementById("wcDisplay");
 
 let bowHP = document.getElementById("bowHP");
@@ -50,3 +50,117 @@ let bridgeHP = document.getElementById("bridgeHP");
 let bilgeHP = document.getElementById("bilgeHP");
 let mastHP = document.getElementById("mastHP");
 let rudderHP = document.getElementById("rudderHP");
+
+let alDisplay = document.getElementById("alDisplay");
+
+let cpDisplay = document.getElementById("cpDisplay");
+let fsDisplay = document.getElementById("fsDisplay");
+let fpDisplay = document.getElementById("fpDisplay");
+
+let wlDisplayB = document.getElementById("wlDisplayB");
+let wlDisplayP = document.getElementById("wlDisplayP");
+let wlDisplayS = document.getElementById("wlDisplayS");
+
+let BwpnSlt1 = document.getElementById("BwpnSlt1");
+let BwpnSlt2 = document.getElementById("BwpnSlt2");
+let BwpnSlt3 = document.getElementById("BwpnSlt3");
+let BwpnSlt4 = document.getElementById("BwpnSlt4");
+let BwpnSlt5 = document.getElementById("BwpnSlt5");
+let BwpnSlt6 = document.getElementById("BwpnSlt6");
+
+let PwpnSlt1 = document.getElementById("PwpnSlt1");
+let PwpnSlt2 = document.getElementById("PwpnSlt2");
+let PwpnSlt3 = document.getElementById("PwpnSlt3");
+let PwpnSlt4 = document.getElementById("PwpnSlt4");
+let PwpnSlt5 = document.getElementById("PwpnSlt5");
+let PwpnSlt6 = document.getElementById("PwpnSlt6");
+
+let SwpnSlt1 = document.getElementById("SwpnSlt1");
+let SwpnSlt2 = document.getElementById("SwpnSlt2");
+let SwpnSlt3 = document.getElementById("SwpnSlt3");
+let SwpnSlt4 = document.getElementById("SwpnSlt4");
+let SwpnSlt5 = document.getElementById("SwpnSlt5");
+let SwpnSlt6 = document.getElementById("SwpnSlt6");
+
+let roundDisplay = document.getElementById("roundDisplay");
+let grapeDisplay = document.getElementById("grapeDisplay");
+let chainDisplay = document.getElementById("chainDisplay");
+
+
+// get all elements that are to be changed by the user
+let shipName = document.getElementById("shipName");
+
+let arType = document.getElementById("armorType");
+let arArea = document.getElementById("armorArea");
+let addArBut = document.getElementById("addArmorButton");
+
+let iconSel = document.getElementById("iconSelect");
+
+let wpnDisB = document.getElementById("wpnDisB");
+let wpnDisP = document.getElementById("wpnDisP");
+let wpnDisS = document.getElementById("wpnDisS");
+
+let wpnLbB = document.getElementById("weaponLbB");
+let wpnTypeB = document.getElementById("weaponTypeB");
+let wpnAddB = document.getElementById("addWeaponButtonB");
+
+let wpnLbP = document.getElementById("weaponLbP");
+let wpnTypeP = document.getElementById("weaponTypeP");
+let wpnAddP = document.getElementById("addWeaponButtonP");
+
+let wpnLbS = document.getElementById("weaponLbS");
+let wpnTypeS = document.getElementById("weaponTypeS");
+let wpnAddS = document.getElementById("addWeaponButtonS");
+
+let roundAdd = document.getElementById("roundAdd");
+let grapeAdd = document.getElementById("grapeAdd");
+let chainAdd = document.getElementById("chainAdd");
+
+// set base values when tonnage changes
+function setBaseVals () {
+    // set base vars
+    let wcBase = statsByTon[parseInt(tonnageTable.value)][1];
+
+    let cpBase = statsByTon[parseInt(tonnageTable.value)][14];
+    let fspBase = statsByTon[parseInt(tonnageTable.value)][15];
+    let fpBase = statsByTon[parseInt(tonnageTable.value)][16];
+
+    let brHPBase = statsByTon[parseInt(tonnageTable.value)][2];
+    let boHPBase = statsByTon[parseInt(tonnageTable.value)][3];
+    let aHPBase = statsByTon[parseInt(tonnageTable.value)][4];
+    let pHPBase = statsByTon[parseInt(tonnageTable.value)][5];
+    let sHPBase = statsByTon[parseInt(tonnageTable.value)][6];
+    let biHPBase = statsByTon[parseInt(tonnageTable.value)][7];
+    let mHPBase = statsByTon[parseInt(tonnageTable.value)][8];
+    let rHPBase = statsByTon[parseInt(tonnageTable.value)][9];
+    
+    let armorRemainingBase = statsByTon[parseInt(tonnageTable.value)][10];
+    let weaponsRemainingBBase = statsByTon[parseInt(tonnageTable.value)][11];
+    let weaponsRemainingPBase = statsByTon[parseInt(tonnageTable.value)][12];
+    let weaponsRemainingSBase = statsByTon[parseInt(tonnageTable.value)][13];
+
+    // update displays
+    wcDisplay.textContent = wcBase;
+
+    bowHP.textContent = boHPBase;
+    aftHP.textContent = aHPBase;
+    portHP.textContent = pHPBase;
+    sbHP.textContent = sHPBase;
+
+    bridgeHP.textContent = brHPBase;
+    bilgeHP.textContent = biHPBase;
+    mastHP.textContent = mHPBase;
+    rudderHP.textContent = rHPBase;
+
+    alDisplay.textContent = armorRemainingBase;
+
+    cpDisplay.textContent = cpBase;
+    fsDisplay.textContent = fspBase;
+    fpDisplay.textContent = fpBase;
+
+    wlDisplayB.textContent = weaponsRemainingBBase;
+    wlDisplayP.textContent = weaponsRemainingPBase;
+    wlDisplayS.textContent = weaponsRemainingSBase;
+}
+
+tonnageTable.addEventListener("blur", setBaseVals());
